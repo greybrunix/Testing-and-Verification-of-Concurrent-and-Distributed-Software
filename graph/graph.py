cb8469ef839458142d609db1afe9665c74613d7b
Pereirics <joao.lc.pereira1@gmail.com>
2024-04-02 11:30:10 +0000

    Só sends que tem receives

--- graph/graph.py
+++ graph/graph.py
@@ -1,9 +1,11 @@
 import re
+import json
 import sys
 import os
 import subprocess
 import networkx as nx
 import matplotlib.pyplot as plt
+
 
 def run_harmony(name):
     pid = os.fork()
@@ -14,15 +16,37 @@ def run_harmony(name):
         name = name[:-3] + "hco"
         run_graph(name)
 
+
+def dfs(edges, node, trace):
+    while True:
+        if not edges[node]:
+            if not trace:
+                return []
+            else:
+                prev_node, sym = trace.pop()
+                #edges[prev_node].append((node, sym))
+                node = prev_node
+        else:
+            neighbour, sym = edges[node].pop()
+            trace.append((node, sym))
+            if neighbour in final:
+                trace.append((neighbour, -1))
+                return trace
+            else:
+                node = neighbour
+    return trace
+
+
 def run_graph(nt, events):
-    # (src, dst, msg, ts, td)
+    # (send, src, dst, msg, ts, td)
+    # (receive, dst, ts, td)
 
     G = nx.DiGraph()
     nodelist = []
     lasts = []
     lasts_cont = []
 
-    for i in range(nt):
+    for i in range(1, nt+1):
         node = f'{i}0'
         G.add_node(node, pos=(i, 0))
         lasts.append(node)
@@ -31,39 +55,43 @@ def run_graph(nt, events):
     
     # Time in the future will determine the y coordinate of each node
     time = 0
-    for ev in events:
+    for ev in events[:]:
         # Time source and dest
-        ts = ev[3]
-        td = ev[4]
+        #ts = ev[3]
+        #td = ev[4]
+        ts = time
+        td = time + 1
+       
+        if ev[0] == 'send' and ('receive', ev[1]) in events:
+            events.remove(('receive', ev[1]))
+            # Update last node in each thread
+            lasts_cont[int(ev[1])-1] += 1
+            lasts_cont[int(ev[2])-1] += 1
         
-        # Update last node in each thread
-        lasts_cont[ev[0]] += 1
-        lasts_cont[ev[1]] += 1
+            # Create node label
+            src = f'{ev[1]}{lasts_cont[int(ev[1])-1]}'
+            dst = f'{ev[2]}{lasts_cont[int(ev[2])-1]}'
         
-        # Create node label
-        src = f'{ev[0]}{lasts_cont[ev[0]]}'
-        dst = f'{ev[1]}{lasts_cont[ev[1]]}'
-        
-        # Add nodes to the list of visible nodes
-        nodelist.append(src)
-        nodelist.append(dst)
+            # Add nodes to the list of visible nodes
+            nodelist.append(src)
+            nodelist.append(dst)
 
-        # Add src and dst nodes to graph
-        G.add_node(src, pos=(ev[0], ts))
-        G.add_node(dst, pos=(ev[1], td))
+            # Add src and dst nodes to graph
+            G.add_node(src, pos=(int(ev[1]), ts))
+            G.add_node(dst, pos=(int(ev[2]), td))
         
-        # Add edge between src and dst, edge to expand each process
-        G.add_edge(lasts[ev[0]], src)
-        G.add_edge(lasts[ev[1]], dst)
-        G.add_edge(src, dst, label=ev[2])
+            # Add edge between src and dst, edge to expand each process
+            G.add_edge(lasts[int(ev[1])-1], src)
+            G.add_edge(lasts[int(ev[2])-1], dst)
+            G.add_edge(src, dst, label=ev[3])
 
         # Update global time
         time = td
     
-    for i in range(nt):
+    for i in range(1, nt+1):
         node = f'{i}n'
-        G.add_node(node, pos=(i, time+0.5))
-        G.add_edge(lasts[i], node)        
+        G.add_node(node, pos=(i, time+1))
+        G.add_edge(lasts[i-1], node)        
 
     # Draw the graph
     pos = nx.get_node_attributes(G, 'pos')
@@ -103,4 +131,37 @@ threads = [(0, 1, "ola", 0.2, 0.5),
 #    run_harmony(name)
 #elif re.search(pattern_hco, name):
 #    run_graph(name)
-run_graph(3, threads)
+
+with open("../lib/algo.hfa", "r") as f:
+    data = json.load(f)
+
+symbols = data.get("symbols")
+symbols_values = [
+    tuple(item['value'][i]['value'] for i in range(len(item['value'])))
+    for item in data['symbols']
+]
+
+nt = max([int(value) for tup in symbols_values 
+          for value in tup[1:] 
+          if value.isdigit()]
+         )
+
+initial = data.get("initial")
+final = [item["idx"] for item in data['nodes'] if item['type'] == 'final']
+edges = {item['src']: [] for item in data['edges']}
+
+for item in data['edges']:
+    src = item['src']
+    dst = item['dst']
+    sym = int(item['sym'])
+    edges[src].append((dst, sym))
+
+node = initial
+visited = set()
+trace = dfs(edges, node, trace)
+events = []
+
+for (n, t) in trace[:-1]:
+    events.append(symbols_values[t])
+
+run_graph(nt, events)
