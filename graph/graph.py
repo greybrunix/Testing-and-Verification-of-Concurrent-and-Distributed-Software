7039c2059a9c098d2fd7d7a905310a3442509103
Pereirics <joao.lc.pereira1@gmail.com>
2024-04-16 13:00:38 +0100

    Done ig

--- graph/graph.py
+++ graph/graph.py
@@ -47,6 +47,7 @@ def run_graph(nt, events):
     nodelist = []
     node_no_label = []
     node_no_receive = []
+    node_self = []
     lasts = []
     lasts_cont = []
 
@@ -64,7 +65,11 @@ def run_graph(nt, events):
         td = time + 1
        
         if ev[0] == 'send' and ('receive', ev[2], ev[4]) in events:
-            events.remove(('receive', ev[2], ev[4]))
+            
+            receive = ('receive', ev[2], ev[4]) 
+            index = events.index(receive)
+            events.remove(receive)
+
             # Update last node in each thread
             lasts_cont[int(ev[1])] += 1
             lasts_cont[int(ev[2])] += 1
@@ -79,7 +84,7 @@ def run_graph(nt, events):
 
             # Add src and dst nodes to graph
             G.add_node(src, pos=(int(ev[1]), ts))
-            G.add_node(dst, pos=(int(ev[2]), td))
+            G.add_node(dst, pos=(int(ev[2]), 1+index))
         
             # Add edge between src and dst, edge to expand each process
             G.add_edge(lasts[int(ev[1])], src)
@@ -96,11 +101,18 @@ def run_graph(nt, events):
         
             # Add nodes to the list of visible nodes
             nodelist.append(src)
+            node_no_receive.append(src)
             node_no_receive.append(dst)
 
-            # Add src and dst nodes to graph
-            G.add_node(src, pos=(int(ev[1]), ts))
-            G.add_node(dst, pos=(int(ev[2])-0.5, td-(td-ts)/2))
+            if src == dst:
+                node_self.append(src)
+                # Add src and dst nodes to graph
+                G.add_node(src, pos=(int(ev[1]), ts))
+                G.add_node(dst, pos=(int(ev[2]), td-(td-ts)/2))
+            else:
+                # Add src and dst nodes to graph
+                G.add_node(src, pos=(int(ev[1]), ts))
+                G.add_node(dst, pos=(int(ev[2])-0.5, td-(td-ts)/2))
         
             # Add edge between src and dst, edge to expand each process
             G.add_edge(lasts[int(ev[1])], src)
@@ -116,33 +128,50 @@ def run_graph(nt, events):
 
     # Draw the graph
     pos = nx.get_node_attributes(G, 'pos')
-    node_attributes = {node: {'visible': False, 'arrow': True} if (node not in nodelist and node in node_no_receive) else {'visible': False, 'arrow': False} if node not in nodelist else {'visible': True, 'arrow': True} for node in G.nodes()}
+    node_attributes = {node: {'visible': False, 'arrow': True} if (node not in nodelist and node in node_no_receive) else {'visible': False, 'arrow': False} if node not in nodelist else {'visible': True, 'arrow': False} if not (node in nodelist and node in node_no_receive) else {'visible': True, 'arrow': True} for node in G.nodes()}
     nx.set_node_attributes(G, node_attributes)
 
-    visible_nodes = [node for node, attrs in G.nodes(data=True) if attrs['visible'] and attrs['arrow']]
-    visible_arrows = [node for node, attrs in G.nodes(data=True) if attrs['arrow'] and not attrs['visible']]
-    
+    visible_nodes = [node for node, attrs in G.nodes(data=True) if attrs['visible'] and node not in node_self]
+    visible_arrows = [node for node, attrs in G.nodes(data=True) if attrs['arrow'] and node not in node_self]
+    red_nodes = [node for node, attrs in G.nodes(data=True) if node in node_self]
+
     # Draw only the visible nodes
     G_visible = G.subgraph(visible_nodes)
 
     # Draw only invisible nodes with arrows
     G_arrows = G.subgraph(visible_arrows)
 
+    G_red = G.subgraph(red_nodes)
+    
     # Draw all nodes without labels
     nx.draw(G_visible, pos, with_labels=False, arrows=True)
 
+    nx.draw_networkx_nodes(G_red, pos, node_color="#ff0000")
+
     # Draw labels for visible edges
     edge_labels = nx.get_edge_attributes(G_visible, 'label')
-    nx.draw_networkx_edge_labels(G_visible, pos, edge_labels=edge_labels)
+    for edge, label in edge_labels.items():
+        nx.draw_networkx_edge_labels(G_visible, pos, edge_labels={edge: label}, label_pos=0.9, verticalalignment='bottom', font_size=10)
+
     arrow_labels = nx.get_edge_attributes(G_arrows, 'label')
-    nx.draw_networkx_edge_labels(G_arrows, pos, edge_labels=arrow_labels)
+    for edge, label in arrow_labels.items():
+        nx.draw_networkx_edge_labels(G_arrows, pos, edge_labels={edge: label}, label_pos=0.8, verticalalignment='bottom', font_size=10)
+
+    red_labels = nx.get_edge_attributes(G_red, 'label')
+    for edge, label in red_labels.items():
+        nx.draw_networkx_edge_labels(G_red, pos, edge_labels={edge: label}, label_pos=0.8, verticalalignment='top', font_size=10)
 
     # Draw edges with invisible nodes
-    invisible_edges = [(u, v) for u, v in G.edges() if u not in visible_nodes or v not in visible_nodes]
+    invisible_edges = [(u, v) for u, v in G.edges() if u not in visible_nodes and v not in visible_nodes and u not in red_nodes and v not in red_nodes]
+    invisible_edges = set(invisible_edges)
     nx.draw_networkx_edges(G, pos, edgelist=invisible_edges, arrows=False)
 
-    visible_edges = [(u, v) for u, v in G.edges() if u in visible_arrows or v in visible_arrows]
+    visible_edges = [(u, v) for u, v in G.edges() if u in visible_arrows and v in visible_arrows and u not in red_nodes and v not in red_nodes]
+    visible_edges = set(visible_edges)
     nx.draw_networkx_edges(G_arrows, pos, edgelist=visible_edges, arrows=True, node_size=1)
+
+    red_edges = [(u, v) for u, v in G.edges() if u in red_nodes and v in red_nodes]
+    nx.draw_networkx_edges(G_red, pos, edgelist=red_edges, arrows=True, connectionstyle='arc3, rad=0.6')
 
     plt.savefig("teste.png")
     plt.show()
@@ -182,7 +211,10 @@ msgs = [('send', 0, 1, 'False', 0),
         ('receive', 1, 0),
         ('send', 0, 1, 'True', 1),
         ('send', 0, 1, 'False', 2),
-        ('receive', 1, 2)
+        ('receive', 1, 2),
+        ('receive', 1, 1), 
+        ('send', 0, 1, 'True', 3),
+        ('send', 0, 0, 'False', 4)
         ]
 
 run_graph(nt, msgs)
